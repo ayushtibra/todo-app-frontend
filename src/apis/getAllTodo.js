8bfeb15bd0dc7fee5c2f0d746beb58bccf92beb9
Ayush Tibra <aayushtibra1997@gmail.com>
2023-10-28 13:06:28 +0530

    Update getAllTodo.js

--- src/apis/getAllTodo.js
+++ src/apis/getAllTodo.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'https://todo-app-3ml2.onrender.com/'
+const baseURL = 'https://todo-app-backend-94be.onrender.com/'
 
 export const getAllTodo = (setToDo) => {
     axios.get(baseURL).then(({ data }) => {
