a82f0ae8da65585b26fe2f04e3db61c0645ecba1
Ayush Tibra <aayushtibra1997@gmail.com>
2023-10-28 13:06:41 +0530

    Update updateTodo.js

--- src/apis/updateTodo.js
+++ src/apis/updateTodo.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getAllTodo } from './getAllTodo';
 
-const baseURL = 'https://todo-app-3ml2.onrender.com/'
+const baseURL = 'https://todo-app-backend-94be.onrender.com/'
 
 export const updateTodo = (todoId, text, setText, setIsUpdate, setToDo) => {
     axios.post(`${baseURL}update`, { _id: todoId, text }).then((data) => {
