51d81a3f960ac28a62b7e26a27dfbfa1ef779f07
Ayush Tibra <aayushtibra1997@gmail.com>
2023-10-28 13:06:12 +0530

    Update deleteTodo.js

--- src/apis/deleteTodo.js
+++ src/apis/deleteTodo.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { getAllTodo } from "./getAllTodo";
 
-const baseURL = `https://todo-app-3ml2.onrender.com/`
+const baseURL = `https://todo-app-backend-94be.onrender.com/`
 
 export const deleteTodo = (id, setToDo) => {
     axios.post(`${baseURL}delete`, { _id: id }).then((data) => {
